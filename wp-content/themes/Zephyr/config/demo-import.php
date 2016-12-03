596ec95097c3b4412f578d19ab8cccfcb9fbc3ff
Pierre Wattré <pierre@wattre.eu>
2016-12-03 12:58:45 +0100

    install Zephyr theme

---
+++ wp-content/themes/Zephyr/config/demo-import.php
@@ -0,0 +1,28 @@
+<?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
+/**
+ * Theme's demo-import settings
+ *
+ * @filter us_config_demo-import
+ */
+return array(
+	'main' => array(
+		'title' => 'Main Demo',
+		'preview_url' => 'http://zephyr.us-themes.com/',
+		'front_page' => 'Home',
+		'nav_menu_locations' => array(
+			'us_main_menu' => 'Main',
+			'us_footer_menu' => 'Secondary',
+		),		
+		'sliders' => array(
+			// Keep the order: second should go first because of VC's hiding default value
+			'slider-second.zip',
+			'slider-main.zip',
+		),
+		'sidebars' => array(
+			'us_widget_area_shop_sidebar' => 'Shop Sidebar',
+			'us_widget_area_login_widget' => 'Login',
+		),
+		'woocommerce' => TRUE,
+	),
+);
